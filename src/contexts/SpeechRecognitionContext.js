7d442f98827c4d7c5857c11e49c796911699df5a
cango91 <can.gologlu@gmail.com>
2023-09-16 14:17:29 -0400

    Add speechRecognitionContext to check for usable sr support

---
+++ src/contexts/SpeechRecognitionContext.js
@@ -0,0 +1,40 @@
+const { createContext, useContext, useState, useEffect } = require("react");
+
+
+const SpeechRecognitionContext = createContext();
+
+export function useSpeechRecognition(){
+    const context = useContext(SpeechRecognitionContext);
+    if(!context){
+        throw new Error('useSpeechRecognition must be used within a SpeechRecognitionProvider');
+    }
+    return context;
+}
+
+export function SpeechRecognitionProvider({children}){
+    const [hasSRSupport, setHasSRSupport] = useState(false);
+    const [recognition,setRecognition] = useState(null);
+
+    useEffect(()=>{
+        let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        let recognition;
+        try{
+            recognition = new SpeechRecognition();
+            setHasSRSupport(true);
+            setRecognition(recognition);
+        }catch(error){
+            setHasSRSupport(false);
+        }
+
+    },[]);
+
+    const value = {
+        hasSRSupport,
+        recognition,
+    }
+    return (
+        <SpeechRecognitionContext.Provider value={value}>
+            {children}
+        </SpeechRecognitionContext.Provider>
+    );
+}
