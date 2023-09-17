170ffc4fbb368a94fad22f5c594ac55e4a8d3869
sivaShilpa <shilpa.vchand@gmail.com>
2023-09-17 16:21:48 -0400

    home page complete

---
+++ src/components/Dealcard/DealCard.js
@@ -0,0 +1,22 @@
+import 'semantic-ui-css/semantic.min.css'
+// import { Item } from 'semantic-ui-react'
+
+export function DealCard(){
+    return(
+        //  <Item.Group link>
+        //     <Item>
+        //         <Item.Image size='large' src='#' />
+        //         <Item.Content>
+        //             <Item.Header>Name of food</Item.Header>
+        //             <Item.Description>food description</Item.Description>
+        //         </Item.Content>
+        //     </Item>
+        //  </Item.Group>
+        <div>
+            
+        </div>
+    )
+}
+
+
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,81 @@
-import {Header} from '../../components/Header/Header';
-import {Footer} from '../../components/Footer/Footer';
-import {DealCard} from '../../components/DealCard';
 import 'semantic-ui-css/semantic.min.css'
-import { Input, Icon } from 'semantic-ui-react'
+import './Home.css'
+import React, { useState, useEffect } from 'react'
 
 export function Home(){
-    return(
-        <>
-            <Header />
-            <div>
-                <div className='search-container'>
-                    <Input icon placeholder='Search for food...'>
-                        <input />
-                        <Icon name='microphone' />
-                    </Input>
+    const [dealMenu, setDealMenu] = useState([]);
+    const BASE_URL = "http://localhost:4000/Menu";
+
+    useEffect(() => {
+        async function fetchData() { 
+          try {
+            const response = await fetch(BASE_URL)
+            dealMenu = await response.filter((i) => {if(i.discount) return i}).json()
+            if(response.ok){
+                setDealMenu([...dealMenu])
+            }
+          }catch(err){
+              console.log(err)
+          }  
+        }    
+        fetchData();
+      }, []);
+    
+    return(        
+        <div className='home'>
+            <div className='search-container'>
+                <div>
+                    <input type="text" className='searchbar' name="searchbar" placeholder="Search for our menu..."/>
                 </div>
-                <br />
-                <div className='deals'>
-                    <h3>Ongoing Deals</h3>
-                    <ul>
-                        <li><DealCard /></li>
-                        <li><DealCard /></li>
-                        <li><DealCard /></li>                        
-                    </ul>
-                </div>
+                <div>
+                    <button className="microphone" onClick={""}><i class="fa fa-microphone"></i></button>
+                </div>                    
             </div>
-            <Footer />
-        </>
+            <div className='deals'>
+                <h1>Ongoing Deals</h1>
+                <div className="eachMenu">
+                    {dealMenu && dealMenu.map((meal)=>(
+                    <div className="menuItem">
+                        <h1 key={meal._id}>{meal.name}</h1>
+                        <img className="mealImage" src={meal.image} alt=""/>
+                    </div>
+                    ))}
+                </div>                
+            </div>
+        </div>        
     )
 }
+
+/**
+ * const [showGood, setShowGood] = useState(false);
+const [menus, setMenus] = useState([]);
+
+  // Simulate fetch data from API
+  useEffect(() => {
+    async function fetchData() {
+      // After fetching data with axios or fetch api
+      // We process the data
+      const goodMenus = dataFromAPI.filter((i) => i.taste === "Good");
+      const restOfMenus = dataFromAPI.filter((i) => i.taste !== "Good");
+
+      // Combine two arrays into one using spread operator
+      // Put the good ones to the front of the array
+      setMenus([...goodMenus, ...restOfMenus]); 
+    }
+
+    fetchData();
+  }, []);
+
+return (
+  <div>
+    // Create a checkbox (you can change it to a toggle button)
+    <input type="checkbox" onChange={() => setShowGood(!showGood)} /> 
+
+    // Conditionally pass in menu data based on the value of toggle button "showGood"
+    <Table
+      data={showGood ? menus : menus.filter((i) => i.taste !== "Good")}
+    />
+  </div>
+);
+
+ */
