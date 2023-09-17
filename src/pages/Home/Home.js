bb1457d8718ba04bb36a63fe398f33273ac243d3
sivaShilpa <shilpa.vchand@gmail.com>
2023-09-17 11:55:28 -0400

    home page wireframe

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,1 +1,32 @@
-export function Home(props){}
+import Header from '../components/Header';
+// import Footer from '../components/Footer';
+import DealCard from '../components/DealCard';
+import { Input, Icon } from 'semantic-ui-react'
+
+export default function Home(){
+
+    return(
+        <>
+        
+        <Header />
+        <div className='search-container'>
+        <Input icon placeholder='Search for food...'>
+            <input />
+            <Icon name='microphone' />
+        </Input>
+        </div>
+        <br />
+        <div className='deals'>
+            <h3>Ongoing Deals</h3>
+            <ul>
+                <li><DealCard /></li>
+                
+            </ul>
+        </div>
+
+        {/* <Footer /> */}
+        
+
+        </>
+    )
+}
