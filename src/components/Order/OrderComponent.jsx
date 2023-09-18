1b6338511657f7ac3eed1713dd78058442f0551a
cango91 <can.gologlu@gmail.com>
2023-09-18 00:37:35 -0400

    orders

--- src/components/Order/OrderComponent.jsx
+++ src/components/Order/OrderComponent.jsx
@@ -1,3 +1,45 @@
-export default function OrderComponent({stage}){
-    
+import { useEffect, useState } from "react";
+import { getCart } from "../../utilities/orders-api";
+
+export default function OrderComponent({ mode }) {
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        const fetchCart = async () => {
+            try {
+                const cart = await getCart();
+                setCart(cart);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchCart();
+    }, []);
+    return (
+        <>
+            {
+                mode === "cart" &&
+                <>
+                    {
+                        cart.orderItems.length && cart.orderItems.map((orderItem) => {
+                            return (
+                                <>
+                                    <div>{orderItem.menuItem.name}</div>
+                                    <div>{orderItem.quantity}</div>
+                                </>
+                            );
+                        })
+                    }
+                </>
+            }
+            {
+                mode === "checkout" &&
+                <>
+                </>
+            }
+            {mode === "summary" &&
+                <>
+                </>
+            }
+        </>
+    );
 }
