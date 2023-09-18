fb77f36c305c28a0cf676851b6c2a96e457eb852
sivaShilpa <shilpa.vchand@gmail.com>
2023-09-18 12:51:17 -0400

    header position fix

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import './SignIn'
 export function SignIn(props) {
 
     const handleGoogleLogin = async () => {
@@ -14,7 +14,7 @@ export function SignIn(props) {
 
     return (
         <>
-            <Link to="#!" onClick={handleGoogleLogin}>Login with google</Link>
+            <Link to="#!" onClick={handleGoogleLogin} className="loginButton">Login with google</Link>
         </>
     );
 }
