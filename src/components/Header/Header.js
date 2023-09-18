693a999f521a610182d6adc2b1a98e7bca253ccd
cango91 <can.gologlu@gmail.com>
2023-09-18 13:00:24 -0400

    logout functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,31 @@
 import "./Header.css"
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getToken, getUser, logOut } from "../../utilities/user-service";
 
 
-export function Header(props) {    
+export function Header(props) {  
+  const [jwt, setJwt] = useState(null);
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if(jwt){
+      if(!getToken()) setJwt(null);
+    }else{
+      if(getToken()) setJwt(getToken());
+    }
+  },[jwt]);
+
+  const logout = () =>{
+    logOut();
+    setJwt(getToken());
+    navigate('/');
+  }
+
   return (    
     <header>      
       <nav className="navbar">      
-        <Link to="/signin"><button className="signIn">Sign In</button></Link>
+        {!jwt && <Link to="/signin"><button className="signIn">Sign In</button></Link>}
+        {jwt && <Link to="" onClick={logout}><button className="signIn">Sign Out</button></Link>}
         <Link to="/cart"><i class="fa fa-shopping-cart"></i></Link>
       </nav>
     </header>     
