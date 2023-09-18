34cc22ee58f186dc847d4b5704394d2189e4d7bf
sivaShilpa <shilpa.vchand@gmail.com>
2023-09-18 15:18:01 -0400

    save2

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -74,20 +74,27 @@ export function Menu(props) {
 
             <div className='search-container'>
                 <div>
-                    <input
-                        type="search"
-                        className='searchbar'
-                        name="searchbar"
+                    <input type="search" 
+                        className='searchbar' 
+                        name="searchbar" 
                         placeholder="Search our menu..."
                         value={searchText}
                         onChange={onChangeSearchText}
                     />
                 </div>
                 <div>
-
-                    <button className="microphone" onClick={""}><svg xmlns="http://www.w3.org/2000/svg" aria-labelledby="title" class="bi bi-mic-fill" viewBox="0 0 16 16"><title id="title">Microphone</title> <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/> <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/> </svg></button>
-                </div>                    
-
+                    <button className="microphone" onClick={""}>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                            aria-labelledby="title" 
+                            class="bi bi-mic-fill" 
+                            viewBox="0 0 16 16">
+                            <title id="title">Microphone</title> 
+                            <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/> 
+                            <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/> 
+                        </svg>
+                    </button>
+                </div>
+                <div>
                     {hasSRSupport && <button className="microphone" onClick={transcribe}>
                         <i class={`fa fa-microphone${transcribing ? "-slash" : ""}`}></i></button>}
                 </div>
