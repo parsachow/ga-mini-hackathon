b9e7fd8ddb0871d70b9163607f58c2c037f0418c
parsachow <parsakishwa06@gmail.com>
2023-09-18 17:11:36 -0400

    cart page update

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,20 @@
 import OrderComponent from "../../components/Order/OrderComponent";
+import "./Cart.css"
+import {Link} from 'react-router-dom'
+
 
 export function Cart(props){
-    return <OrderComponent mode="cart" />
+    return (
+        <>
+        <h2>Items in Cart</h2>
+    <div className='cart-layout'>
+        <OrderComponent mode="cart" />
+    </div>
+    <br />
+    <br />
+    <div>
+        <Link to='/checkout'><button className="cart-btn">Checkout</button></Link>
+    </div>
+    </>
+    )
 }
