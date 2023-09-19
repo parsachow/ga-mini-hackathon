29501388b894d8c259e92dfb6a1c40086c3c1ed0
sivaShilpa <shilpa.vchand@gmail.com>
2023-09-19 13:53:44 -0400

    home page complete

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -78,27 +78,7 @@ export function Home({setCart}){
             <div className="dealAtHome">
                 <Deals/>
             </div>
-        </div>  
-        // <div className='home'>
-        //     <Link to="/menu"><button className="menuButton" type="button">Search our menu</button></Link>
-        //     <div className='deals'>
-        //         <h1>Ongoing Deals</h1>
-        //         <div className="eachMenu">
-        //             {dealMenu && dealMenu.map((meal)=>(
-        //                 <Link to={`/menu/${meal._id}`}>
-        //                     <div className="menuItem">
-        //                         <img className="mealImage" src={meal.imageUrl} alt={meal.imageDescription}/>
-        //                         <h2 className="mealName" key={meal._id}>{meal.name}</h2>
-        //                         <p className="price">
-        //                             <span className='initialPrice'>${meal.price.toFixed(2)}</span>&nbsp;&nbsp;
-        //                             <span>${(meal.price - (meal.price*meal.discount)).toFixed(2)}</span>
-        //                         </p>
-        //                     </div>
-        //                 </Link>
-        //             ))}
-        //         </div>                
-        //     </div>
-        // </div>        
+        </div>         
 
     )
 }
