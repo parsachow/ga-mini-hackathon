3a4263f0077bf6c439517a532923479ba55d2daa
cango91 <can.gologlu@gmail.com>
2023-09-19 14:54:35 -0400

    cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -9,6 +9,7 @@ import { getCart } from "../../utilities/orders-api"
 
 export function Cart(props){
     const [cart, setCart] = useState([]);
+
     useEffect(() => {
         const fetchCart = async () => {
             try {
@@ -25,8 +26,20 @@ export function Cart(props){
         <>
         <h2>Items in Cart</h2>
         <div className='cart-layout'>
-           {cart && cart.map(item => {
-            return MenuItem.name
+           {cart && cart.orderItems && cart.orderItems.map(orderItem => {
+            const item=orderItem.menuItem;
+            return <MenuItem
+            showFavIcon={false}
+            itemImgUrl={item.imageUrl}
+            itemImgAlt={item.imageDescription}
+            itemName={item.name}
+            itemDescription={item.description}
+            itemPrice={item.price}
+            btnText="remove item from cart"
+            btnImg="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNyIgaGVpZ2h0PSIyNyIgdmlld0JveD0iMCAwIDI3IDI3IiBmaWxsPSJub25lIj4KICA8cGF0aCBkPSJNMjIuODc1IDEwLjY0MDZMMjAuNzkxNyAyNi4wMDA3SDYuMjA4MzNMNC4xMjUgMTAuNjQwNiIgc3Ryb2tlPSIjMDAwMDAxIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgogIDxwYXRoIGQ9Ik0xIDYuODA0NjlIMjYiIHN0cm9rZT0iIzAwMDAwMSIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KICA8cGF0aCBkPSJNOC4yMDgwMSA2LjI0MTYxVjIuOTIwMDFDOC4yMDgwMSAyLjQxMDc5IDguNDI3NSAxLjkyMjQzIDguODE4MiAxLjU2MjM2QzkuMjA4OSAxLjIwMjI5IDkuNzM4ODEgMSAxMC4yOTEzIDFIMTYuNTQxM0MxNy4wOTM5IDEgMTcuNjIzOCAxLjIwMjI5IDE4LjAxNDUgMS41NjIzNkMxOC40MDUyIDEuOTIyNDMgMTguNjI0NyAyLjQxMDc5IDE4LjYyNDcgMi45MjAwMVY2Ljc2MDAyIiBzdHJva2U9IiMwMDAwMDEiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+Cjwvc3ZnPg==" 
+            itemId={item._id}
+            tabindex={1} />
+
            } )}
         </div>
         <br />
