91955b1f3720496ca050025fdd9228adb24860b5
parsachow <parsakishwa06@gmail.com>
2023-09-19 14:44:16 -0400

    cart updates

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,20 +1,42 @@
-import OrderComponent from "../../components/Order/OrderComponent";
+import MenuItem from "../../components/MenuItem/MenuItem"
 import "./Cart.css"
 import {Link} from 'react-router-dom'
+import { useEffect, useState } from "react"
+import { getCart } from "../../utilities/orders-api"
+
+
 
 
 export function Cart(props){
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        const fetchCart = async () => {
+            try {
+                const cart = await getCart();
+                console.log(cart);
+                setCart(cart);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchCart();
+    }, []);
     return (
         <>
         <h2>Items in Cart</h2>
-    <div className='cart-layout'>
-        <OrderComponent mode="cart" />
-    </div>
-    <br />
-    <br />
-    <div>
-        <Link to='/checkout'><button className="cart-btn">Checkout</button></Link>
-    </div>
+        <div className='cart-layout'>
+           {cart && cart.map(item => {
+            return MenuItem.name
+           } )}
+        </div>
+        <br />
+        <br />
+        <div className="buttons">
+            <Link to='/checkout'><button className="cart-btn">Checkout</button></Link>
+            <br />
+            <br />
+            <Link to='/'><button className="cart-btn">Add More Items</button></Link>
+         </div>
     </>
     )
 }
