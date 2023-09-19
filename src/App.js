b8d42d9d6d2f4db6425b3773a42c4a281fae96c5
parsachow <parsakishwa06@gmail.com>
2023-09-19 01:47:10 -0400

    sidebar and header update

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import {Home} from "./pages/Home/Home"
 import {Settings} from "./pages/Settings/Settings"
 import {Menu} from "./pages/Menu/Menu"
 import {SignIn} from "./pages/SignIn/SignIn"
-
+import Navbar from "./components/SidebarNav/SidebarNav";
 import {Cart} from "./pages/Cart/Cart"
 import {Checkout} from "./pages/Checkout/Checkout"
 import {Details} from "./pages/Details/Details"
 import {Favorites} from "./pages/Favorites/Favorites"
 import {Summary} from "./pages/Summary/Summary"
-
+import {React, useState} from 'react'
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import LoginSuccess from "./components/LoginSuccess/LoginSuccess";
-import TestPage from "./pages/TestPage";
+
 
 
 function App() {
-  const URL = "http://localhost:4000/menu";
+
+
   return (
     <div className="App">
-
+      
+      <Header />
+      <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/settings" element={<Settings/>} />
