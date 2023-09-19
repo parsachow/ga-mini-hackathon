f8942ae3e860b10c13eab3cd2722a506e8f5ea5a
cango91 <can.gologlu@gmail.com>
2023-09-19 15:23:42 -0400

    deploy

---
+++ .sync_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+APP_NAME="cafe-normale"
+
+# Path to your .env file
+ENV_FILE="./.env"
+
+# Read each line from the .env file and set the environment variable in Heroku
+while IFS= read -r line || [[ -n "$line" ]]; do
+    # Check if the line is not empty and is not a comment (starts with '#')
+    if [[ -n "$line" && ! "$line" =~ ^\# ]]; then
+        # Split the line into key and value
+        key=$(echo "$line" | cut -d= -f1)
+        value=$(echo "$line" | cut -d= -f2-)
+        
+        # Set the environment variable in Heroku
+        heroku config:set "$key=$value" --app "$APP_NAME"
+        
+        # Print the environment variable that was set (for visual confirmation)
+        echo "Set $key=$value in Heroku."
+    fi
+done < "$ENV_FILE"
