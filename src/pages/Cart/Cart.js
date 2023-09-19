7d673fc1fafee3a2ded04aff06b871533273c527
cango91 <can.gologlu@gmail.com>
2023-09-19 15:08:55 -0400

    delete item from cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -2,7 +2,7 @@ import MenuItem from "../../components/MenuItem/MenuItem"
 import "./Cart.css"
 import {Link} from 'react-router-dom'
 import { useEffect, useState } from "react"
-import { getCart } from "../../utilities/orders-api"
+import { getCart, setItemQtyInCart } from "../../utilities/orders-api"
 
 
 
@@ -22,6 +22,11 @@ export function Cart(props){
         }
         fetchCart();
     }, []);
+
+    const handleRemoveItem = async (itemId) =>{
+        const cart = await setItemQtyInCart(itemId,0);
+        setCart(cart);
+    }
     return (
         <>
         <h2>Items in Cart</h2>
@@ -38,7 +43,8 @@ export function Cart(props){
             btnText="remove item from cart"
             btnImg="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNyIgaGVpZ2h0PSIyNyIgdmlld0JveD0iMCAwIDI3IDI3IiBmaWxsPSJub25lIj4KICA8cGF0aCBkPSJNMjIuODc1IDEwLjY0MDZMMjAuNzkxNyAyNi4wMDA3SDYuMjA4MzNMNC4xMjUgMTAuNjQwNiIgc3Ryb2tlPSIjMDAwMDAxIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgogIDxwYXRoIGQ9Ik0xIDYuODA0NjlIMjYiIHN0cm9rZT0iIzAwMDAwMSIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KICA8cGF0aCBkPSJNOC4yMDgwMSA2LjI0MTYxVjIuOTIwMDFDOC4yMDgwMSAyLjQxMDc5IDguNDI3NSAxLjkyMjQzIDguODE4MiAxLjU2MjM2QzkuMjA4OSAxLjIwMjI5IDkuNzM4ODEgMSAxMC4yOTEzIDFIMTYuNTQxM0MxNy4wOTM5IDEgMTcuNjIzOCAxLjIwMjI5IDE4LjAxNDUgMS41NjIzNkMxOC40MDUyIDEuOTIyNDMgMTguNjI0NyAyLjQxMDc5IDE4LjYyNDcgMi45MjAwMVY2Ljc2MDAyIiBzdHJva2U9IiMwMDAwMDEiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+Cjwvc3ZnPg==" 
             itemId={item._id}
-            tabindex={1} />
+            tabindex={1} 
+            onClick={handleRemoveItem}/>
 
            } )}
         </div>
