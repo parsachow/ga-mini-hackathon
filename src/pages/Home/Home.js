35164e6fe837bb01b318417ef09eb87c293cf002
cango91 <can.gologlu@gmail.com>
2023-09-19 11:17:09 -0400

    Stashing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,16 @@
 import './Home.css'
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import MenuItem from "../../components/MenuItem/MenuItem";
 import SearchBar from '../../components/SearchBar/SearchBar';
 import FoodCategory from '../../components/FoodCategory/FoodCategory';
+import { getUser } from '../../utilities/user-service';
+import { addItemToCart } from '../../utilities/orders-api';
 
 export function Home(){
     const [menu, setMenu] = useState([]);
     const [filteredMenu,setFilteredMenu] = useState([]);
+    const navigate = useNavigate();
     const BASE_URL = "http://localhost:4000/menu";
 
     useEffect(() => {
@@ -40,6 +43,11 @@ export function Home(){
         }
     }
 
+    const handleAddToCart = async itemId =>{
+        if(!getUser()) return navigate('/signin');
+        const cart = await addItemToCart(itemId);
+    }
+
     return(  
         <div className='home'> 
             <img alt="delicious spaghetti with tomatoe sauce" className="topImage" src="https://i.imgur.com/i3LZenx.jpg"></img> 
@@ -61,29 +69,10 @@ export function Home(){
                     btnText="Add"
                     key={item._id}
                     itemId={item._id}
+                    onClick={handleAddToCart}
                 />
             )}
-        </div>  
-        // <div className='home'>
-        //     <Link to="/menu"><button className="menuButton" type="button">Search our menu</button></Link>
-        //     <div className='deals'>
-        //         <h1>Ongoing Deals</h1>
-        //         <div className="eachMenu">
-        //             {dealMenu && dealMenu.map((meal)=>(
-        //                 <Link to={`/menu/${meal._id}`}>
-        //                     <div className="menuItem">
-        //                         <img className="mealImage" src={meal.imageUrl} alt={meal.imageDescription}/>
-        //                         <h2 className="mealName" key={meal._id}>{meal.name}</h2>
-        //                         <p className="price">
-        //                             <span className='initialPrice'>${meal.price.toFixed(2)}</span>&nbsp;&nbsp;
-        //                             <span>${(meal.price - (meal.price*meal.discount)).toFixed(2)}</span>
-        //                         </p>
-        //                     </div>
-        //                 </Link>
-        //             ))}
-        //         </div>                
-        //     </div>
-        // </div>        
+        </div>   
 
     )
 }
