a4f356ffc6dff6fff1756caff04ba7163e503a7f
cango91 <can.gologlu@gmail.com>
2023-09-19 13:52:13 -0400

    merge conflict

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,12 +25,6 @@ export function Header({ cart }) {
   }
 
 
-<<<<<<< HEAD
-  return (    
-    <header>  
-          
-      <nav className="navbar-header"> 
-=======
   return (
     <header>
       <nav className="navbar-header">
@@ -53,7 +47,6 @@ export function Header({ cart }) {
         <div className="title">
           <Link to='/' className="app-title">Cafe Normalé</Link>
         </div>
->>>>>>> main
         <div>
           <span>
             <Link to="/cart"><svg xmlns="http://www.w3.org/2000/svg" aria-labelledby="title-cart" className="bi bi-cart" viewBox="0 0 16 16"> <title id='title-cart'>Shopping Cart</title> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" /> </svg></Link>
