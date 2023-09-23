200df87e16ed325d8b0a0bd4f5323bae4243ec96
sivaShilpa <shilpa.vchand@gmail.com>
2023-09-22 21:40:10 -0400

    category buttons fix

--- src/components/FoodCategory/FoodCategory.js
+++ src/components/FoodCategory/FoodCategory.js
@@ -1,4 +1,5 @@
 import './FoodCategory.css';
+import { useEffect, useState } from 'react';
 import React from 'react';
 import {ReactComponent as AppetizerLogo} from '../../svg files/Group.svg';
 import {ReactComponent as EntreeLogo} from '../../svg files/food-kitchenware-serving-dome--cook-tool-dome-kitchen-serving-paltter-dish-tools-food.svg';
@@ -6,21 +7,26 @@ import {ReactComponent as DessertLogo} from '../../svg files/Vector.svg';
 
 
 
-export default function FoodCategory({ foodCategory }) {
+export default function FoodCategory({ foodCategory, onClick, tabindex }) {
+    const [category, setCategory] = useState("")
+    useEffect(()=>{
+        if(foodCategory) { setCategory(foodCategory)}
+    },[]);
+
     return (
-        <div className="foodCategory">
+        <div className="foodCategory" tabIndex={tabindex || 1}>
             <div className='categoryIcon'>
                 {
-                    foodCategory && foodCategory.toLowerCase()==="appetizer"?<AppetizerLogo />:
-                    foodCategory.toLowerCase() === "entree"?<EntreeLogo className='entree'/>:
-                    foodCategory.toLowerCase() === "dessert"?<DessertLogo/>: null
+                    category && category.toLowerCase()==="appetizer"?<AppetizerLogo className='appetizer'/>:
+                    category.toLowerCase() === "entree"?<EntreeLogo className='entree'/>:
+                    category.toLowerCase() === "dessert"?<DessertLogo/>: null
                 }
             </div>
             <div className='categoryName'>
                 {
-                    foodCategory && foodCategory.toLowerCase()==="appetizer"?"Appetizer":
-                    foodCategory.toLowerCase() === "entree"?"Entree":
-                    foodCategory.toLowerCase() === "dessert"?"Dessert": null
+                    category && category.toLowerCase()==="appetizer"?"Appetizer":
+                    category.toLowerCase() === "entree"?"Entree":
+                    category.toLowerCase() === "dessert"?"Dessert": null
                 }
             </div>
         </div>
