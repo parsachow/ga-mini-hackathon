45f2f28cc075bf8058c2de777d15a9708deaf9cd
parsachow <parsakishwa06@gmail.com>
2023-10-13 12:46:51 -0400

    checkout page updates

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -2,8 +2,19 @@ export function Checkout(props){
 
     return (
         <div className="delivery-main-div">
-        <h2>Delivery Details</h2>
+            <br />
+            <br />
+            <br />
+            <br />
+            <h2>Delivery Details</h2>
+
+            <div>
+                <form>
+                    <textarea name='postAddress' placeholder="Address" wrap="soft" rows={4} cols={40}/>
+                </form>
+            </div>
         
+
         </div>
     )
 }
