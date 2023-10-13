d6e463a1c140420f40d563242693104591fd23ff
cango91 <can.gologlu@gmail.com>
2023-10-13 12:45:07 -0400

    fix home page categories and search. Add deals to cart.

--- src/components/FoodCategory/FoodCategory.js
+++ src/components/FoodCategory/FoodCategory.js
@@ -1,33 +1,19 @@
 import './FoodCategory.css';
 import { useEffect, useState } from 'react';
 import React from 'react';
-import {ReactComponent as AppetizerLogo} from '../../svg files/Group.svg';
-import {ReactComponent as EntreeLogo} from '../../svg files/food-kitchenware-serving-dome--cook-tool-dome-kitchen-serving-paltter-dish-tools-food.svg';
-import {ReactComponent as DessertLogo} from '../../svg files/Vector.svg';
 
 
 
-export default function FoodCategory({ foodCategory, onClick, tabindex }) {
-    const [category, setCategory] = useState("")
-    useEffect(()=>{
-        if(foodCategory) { setCategory(foodCategory)}
-    },[]);
+
+export default function FoodCategory({ categoryName, children, tabindex, toggled, onClick, onKeyPress }) {
 
     return (
-        <div className="foodCategory" tabIndex={tabindex || 1}>
+        <div onKeyDown={onKeyPress} onClick={onClick} className={`foodCategory ${toggled ? "toggled" : ""}`} tabIndex={tabindex || 1}>
             <div className='categoryIcon'>
-                {
-                    category && category.toLowerCase()==="appetizer"?<AppetizerLogo className='appetizer'/>:
-                    category.toLowerCase() === "entree"?<EntreeLogo className='entree'/>:
-                    category.toLowerCase() === "dessert"?<DessertLogo/>: null
-                }
+                {children}
             </div>
             <div className='categoryName'>
-                {
-                    category && category.toLowerCase()==="appetizer"?"Appetizer":
-                    category.toLowerCase() === "entree"?"Entree":
-                    category.toLowerCase() === "dessert"?"Dessert": null
-                }
+                {categoryName}
             </div>
         </div>
     );
